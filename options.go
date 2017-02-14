168c6b943c28cf8a76fa82ebdb890ce2ae2cf561
Abdul Dakkak <abduld@wolfram.com>
2017-02-14 17:58:17 -0600

    Add helper options

--- options.go
+++ options.go
@@ -17,6 +17,30 @@ type Options struct {
 
 type Option func(*Options)
 
+func Endpoints(addrs []string) Option {
+	return func(o *Options) {
+		o.Endpoints = addrs
+	}
+}
+
+func Serializer(s serializer.Serializer) Option {
+	return func(o *Options) {
+		o.Serializer = s
+	}
+}
+
+func Secure(b bool) Option {
+	return func(o *Options) {
+		o.Secure = b
+	}
+}
+
+func TLSConfig(t *tls.Config) Option {
+	return func(o *Options) {
+		o.TLSConfig = t
+	}
+}
+
 type PublishOptions struct {
 	Context context.Context
 }
@@ -30,3 +54,15 @@ type SubscribeOptions struct {
 }
 
 type SubscribeOption func(*SubscribeOptions)
+
+func AutoAck(b bool) SubscribeOption {
+	return func(o *SubscribeOptions) {
+		o.AutoAck = b
+	}
+}
+
+func Queue(s string) SubscribeOption {
+	return func(o *SubscribeOptions) {
+		o.Queue = s
+	}
+}
