b0b727f6e0695b8948de31edcbffe64ddfb1bea7
Abdul Dakkak <dakkak@illinois.edu>
2017-05-04 09:06:57 -0500

    set broker visability

--- sqs/sqs.go
+++ sqs/sqs.go
@@ -188,7 +188,9 @@ func (b *sqsBroker) Subscribe(topic string, handler broker.Handler, opts ...brok
 					MessageAttributeNames: aws.StringSlice([]string{
 						"All",
 					}),
-					WaitTimeSeconds: aws.Int64(timeout),
+					WaitTimeSeconds:   aws.Int64(timeout),
+					VisibilityTimeout: aws.Int64(10 /* seconds */),
+					WaitTimeSeconds:   aws.Int64(5 /* seconds */),
 				})
 				if err != nil {
 					log.Errorf("Unable to receive message from queue %q, %v.", topic, err)
